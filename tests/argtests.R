913227cb04b3ba5031be78a9f6be3ec41d806aa5
Andrea-S-8 <andreastorey29@gmail.com>
2025-08-26 15:43:28 +0100

    Update argtests.R

--- tests/argtests.R
+++ tests/argtests.R
@@ -297,3 +297,115 @@ expect_error(pacf(y, target = ncol(y)), "target must either be NULL, length 1, l
 #Failed as no errors found as expected
 expect_error(pacf(y, target = NULL), "target must either be NULL, length 1, length lag.max, ncol(x), or a matrix with dimension lag.max x nser.")
 #Failed as no errors found as expected
+
+
+
+
+set.seed(12345)
+x1 = arima.sim(model=list(ar=0.4),n=500)
+x2 = arima.sim(model=list(ar=0.8),n=500)
+x=cbind(x1, x2)
+# testing matrix
+
+## testing that PenalizedCorr can only be logical
+### acf ###
+expect_error(acf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(acf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(acf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(acf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### pacf ###
+expect_error(pacf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(pacf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(pacf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(pacf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### ar ###
+expect_error(ar(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(ar(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### auto.acf ###
+expect_error(auto.acf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(auto.acf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(auto.acf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(auto.acf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### invertpacf ###
+expect_error(invertpacf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(invertpacf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(invertpacf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(invertpacf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### DLpencoaf ###
+expect_error(DLpencoef(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(DLpencoef(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(DLpencoef(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(DLpencoef(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### ar.penyw ###
+expect_error(ar.penyw(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar.penyw(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar.penyw(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(ar.penyw(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+
+
+
+## testing that data series can only be numeric
+#### acf ####
+expect_error(acf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### pacf ####
+expect_error(pacf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### auto.acf ####
+expect_error(auto.acf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### invertpacf ####
+expect_error(invertpacf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### DLpencoef ####
+expect_error(DLpencoef(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### ar ####
+expect_error(ar(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### ar.penyw ####
+expect_error(ar.penyw(x), "'x' must be numeric")
+#Failed as no error found as expected
