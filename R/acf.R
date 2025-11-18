150fd9938a2d52c40eb7f888965185d6045568d0
Rebecca Killick <r.killick@lancaster.ac.uk>
2025-11-18 15:36:48 +0000

    Update acf documentation h
    Removed references to h, replaced with lag.max

--- R/acf.R
+++ R/acf.R
@@ -16,7 +16,7 @@
 #' @param na.action function to be called to handle missing values. \code{na.pass} can be used.
 #' @param demean logical. Should a mean be estimated and subtracted before correlations are calculated?
 #' @param penalized logical. If \code{TRUE} (the default) the penalized ACF/PACF is computed; if \code{FALSE} the sample ACF/PACF is computed using \code{stats:acf}.
-#' @param lh sequence of threshold values across h, default is \code{NULL}. Could be a single value (repeated for all h), a single vector of length lag.max (repeated for all nser), or a h x nser matrix. Default is data driven choice.
+#' @param lh sequence of threshold values across 1:lag.max, default is \code{NULL}. Could be a single value (repeated for all 1:lag.max), a single vector of length lag.max (repeated for all nser), or a lag.max x nser matrix. Default is data driven choice.
 #' @param estimate character vector of the estimation method for the ACF, options are \code{"direct"} (default) or \code{"invertpacf"}.  \code{"invertpacf"} is preferred when the data can be approximated by a low order AR model.
 #' @param lambda controls the degree of shrinkage towards the target.
 #' @param target the unbiased (partial) autocorrelation function from a (model) assumption.
